e62b3bff54fdfbaab8fd034e8bef58258f6f16bf
Obaid <obaid@Obaids-MBP.home>
2025-01-23 00:26:48 +0100

    refactor: remove unused code

--- main.go
+++ main.go
@@ -10,11 +10,3 @@ func main() {
 	fmt.Println("Hello World!")
 	log.Printf("Application started at: %v", time.Now())
 }
-
-// func badpractice() {
-
-// 	fmt.Println("Hello World from the bad practice!")
-
-// 	// Adding logs
-// 	log.Printf("Application started at: %v", time.Now())
-// }
