e3800689cfe4a9ee6ea38765f143c996fd4ebf7f
Obaid <obaid@Obaids-MBP.home>
2025-01-28 01:13:42 +0100

    feat: add time tracking of login

--- main.go
+++ main.go
@@ -8,5 +8,12 @@ import (
 
 func main() {
 	fmt.Println("Hello World!")
-	log.Printf("Application started at: %v", time.Now())
+	log.Printf("Application started at: %v", LoginTime("admin"))
+}
+
+func LoginTime(username string) string {
+	currentTime := time.Now()
+	formattedTime := currentTime.Format("2006-01-02 15:04:05")
+	fmt.Printf("User %s logged in at %s\n", username, formattedTime)
+	return formattedTime
 }
