26b42d4bdd861382a15219cf8b00e48923e71425
Obaid <obaid.ghafoori9@gmail.com>
2025-01-31 15:05:16 +0100

    feat: update git hook message

--- install-hooks.sh
+++ install-hooks.sh
@@ -3,8 +3,6 @@
 git config core.hooksPath .githooks
 
 chmod +x .githooks/pre-push
-
-cp scripts/git-hooks/pre-commit .git/hooks/pre-commit
-chmod +x .git/hooks/pre-commit
+chmod +x .githooks/pre-commit
 
 echo "Git hooks installed successfully."
