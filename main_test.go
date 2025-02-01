8b902a210735946e455dc733ecaecdc064e0df95
Obaid <obaid.ghafoori9@gmail.com>
2025-02-01 01:42:59 +0100

    docs: write test and increase test coverage to 80%

--- main_test.go
+++ main_test.go
@@ -32,3 +32,29 @@ func TestPrintHelloWorld(t *testing.T) {
 		t.Errorf("Expected output %q, got %q", expectedOutput, output)
 	}
 }
+func TestPrintUserData(t *testing.T) {
+	// Capture stdout
+	oldStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	// Call printUserData function
+	go func() {
+		printUserData("John Doe", 30, "test#emia.com")
+		w.Close()
+	}()
+	buf := new(bytes.Buffer)
+	_, err := buf.ReadFrom(r)
+	os.Stdout = oldStdout
+
+	if err != nil {
+		t.Fatalf("Failed to read captured output: %v", err)
+	}
+
+	// Test stdout output
+	output := buf.String()
+	expectedOutput := "Name:  John Doe\nAge:  30\nEmail:  test#emia.com\n"
+	if output != expectedOutput {
+		t.Errorf("Expected output %q, got %q", expectedOutput, output)
+	}
+}
